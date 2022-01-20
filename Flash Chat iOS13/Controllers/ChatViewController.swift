6c015eec6fad0460c0a648a793e48cefeb59c940
AlexBitar80 <joaoalexandre.bitar@gmail.com>
2022-01-19 23:25:12 -0300

    displaying messages in real-time and sorting new messages

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -27,9 +27,12 @@ class ChatViewController: UIViewController, UITableViewDataSource {
     }
     
     func loadMessages() {
-        messages = []
-        
-        db.collection(K.FStore.collectionName).getDocuments { querySnapshot, error in
+        db.collection(K.FStore.collectionName)
+            .order(by: K.FStore.dateField)
+            .addSnapshotListener { querySnapshot, error in
+            
+            self.messages = []
+            
             if let e = error {
                 print("There was an retrieving data from FireStore. \(e)")
             } else {
@@ -55,7 +58,8 @@ class ChatViewController: UIViewController, UITableViewDataSource {
         if let messageBody = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email {
             db.collection(K.FStore.collectionName).addDocument(data: [
                 K.FStore.senderField: messageSender,
-                K.FStore.bodyField: messageBody
+                K.FStore.bodyField: messageBody,
+                K.FStore.dateField: Date().timeIntervalSince1970
             ]) { error in
                 if let e = error {
                     print("There was an issue saving data to FireStore, \(e)")
